520d9bcf5635323ea0b4805e9438293aa4b8e00a
Sean Campbell <sean.campbell13@gmail.com>
2023-08-29 07:49:26 -0400

    fix build? add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
-import { defineConfig, sharpImageService } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig, sharpImageService } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 /** @type import('astro').defineConfig */
-import react from '@astrojs/react'
+import react from '@astrojs/react';
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   experimental: {
-    assets: true,
+    assets: true
   },
-  // image: {
-  //   service: sharpImageService(),
-  // },
+  image: {
+    service: sharpImageService(),
+  },
   vite: {
     ssr: {
-      noExternal: ['usehooks-ts'],
-    },
+      noExternal: ['usehooks-ts']
+    }
   },
-})
+  // output: "server",
+  adapter: vercel()
+});
