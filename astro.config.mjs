c8169dd0bf49a73c3ba07b6853d27207999b4f38
Sean Campbell <sean.campbell13@gmail.com>
2023-08-29 08:04:12 -0400

    static build?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 /** @type import('astro').defineConfig */
 import react from '@astrojs/react';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,6 +20,6 @@ export default defineConfig({
       noExternal: ['usehooks-ts']
     }
   },
-  output: "server",
+  output: "static",
   adapter: vercel()
 });
