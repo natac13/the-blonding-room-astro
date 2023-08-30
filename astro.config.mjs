f603ffad4ce801deb0d7ac247ba669e879025ffb
Sean Campbell <sean.campbell13@gmail.com>
2023-08-30 15:09:56 -0400

    upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,22 @@
-import { defineConfig, sharpImageService } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig, sharpImageService } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 /** @type import('astro').defineConfig */
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import vercel from "@astrojs/vercel/static";
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  experimental: {
-    assets: true
-  },
   image: {
     service: sharpImageService(),
   },
   vite: {
     ssr: {
-      noExternal: ['usehooks-ts']
-    }
+      noExternal: ['usehooks-ts'],
+    },
   },
-  output: "static",
-  adapter: vercel()
-});
+  output: 'static',
+  adapter: vercel(),
+})
