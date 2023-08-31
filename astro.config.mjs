9bcc21ad2d8caed1844a40bf0e265baf04cbbca5
Sean Campbell <sean.campbell13@gmail.com>
2023-08-31 19:01:50 -0400

    i was wrong on the last one

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       noExternal: ['usehooks-ts'],
     },
   },
-  site: 'https://theblondingroom.com',
+  // site: 'https://theblondingroom.com',
   output: 'static',
   adapter: vercel(),
 })
