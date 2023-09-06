08d91bbe553ce23ae956f1b8cf1a5f65ff444803
Sean Campbell <sean.campbell13@gmail.com>
2023-09-06 08:41:34 -0400

    feat: eslint prettier tsc setup and gh actions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,28 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
+    // 'plugin:react/recommended',
     'plugin:astro/recommended',
     'prettier',
   ],
   overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ['*.astro'],
+      // Allows Astro components to be parsed.
+      parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        // 'react/react-in-jsx-scope': 'off',
+        // 'react/classname-in-styles': 'off',
+        // 'react/no-unknown-property': ['error', { ignore: ['class'] }],
+      },
+    },
     {
       env: {
         node: true,
@@ -21,22 +38,36 @@ module.exports = {
       },
     },
     {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
+      extends: ['plugin:react/recommended'],
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
       parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
+        project: './tsconfig.json',
+      },
+      rules: {
+        'no-console': 'warn',
+        '@typescript-eslint/triple-slash-reference': 'off',
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
       },
     },
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'react'],
-  rules: {
-    'no-console': 'warn',
-    '@typescript-eslint/triple-slash-reference': 'off',
-  },
+  // settings: {
+  //   react: {
+  //     version: 'detect',
+  //   },
+  // },
+  // parser: '@typescript-eslint/parser',
+  // parserOptions: {
+  //   ecmaVersion: 'latest',
+  //   sourceType: 'module',
+  // },
+  // plugins: ['@typescript-eslint', 'react'],
+  // rules: {
+  //   'no-console': 'warn',
+  //   '@typescript-eslint/triple-slash-reference': 'off',
+  // },
 }
