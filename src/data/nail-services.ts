d03ff172de1900c5100032f2f1afc720ba20698d
Sean Campbell <sean.campbell13@gmail.com>
2023-12-23 19:39:50 -0500

    chore: remove nail pricing

--- src/data/nail-services.ts
+++ src/data/nail-services.ts
@@ -2,7 +2,6 @@ export const NAIL_SERVICES: {
   title: string
   items: {
     item: string
-    price: string
   }[]
 }[] = [
   {
@@ -10,60 +9,47 @@ export const NAIL_SERVICES: {
     items: [
       {
         item: 'Regular Polish Change',
-        price: '$25',
       },
       {
         item: 'Regular Polish Manicure',
-        price: '$35',
       },
       {
         item: 'Gel Polish Change',
-        price: '$35',
       },
       {
         item: 'Gel Polish Manicure',
-        price: '$45',
       },
       {
         item: 'Dip Colour Change',
-        price: '$45',
       },
       {
         item: 'Dip Manicure',
-        price: '$55',
       },
     ],
   },
   {
-    title: 'Artifical',
+    title: 'Artificial',
     items: [
       {
         item: 'Dip Full Set with Tips',
-        price: '$65',
       },
       {
         item: 'Add Cuticle Clean up',
-        price: '$10',
       },
       {
         item: 'Designs on All Nails',
-        price: '$15+',
       },
       {
         item: 'Designs on 2 Nails',
-        price: '$8',
       },
       {
         item: 'Add French',
-        price: '$10',
       },
       {
         item: 'Nails Removal',
-        price: '$20',
       },
       {
         item: 'Nail Repair',
-        price: '$5',
       },
     ],
   },
@@ -72,19 +58,15 @@ export const NAIL_SERVICES: {
     items: [
       {
         item: 'Regular Polish Toe Colour Change',
-        price: '$30',
       },
       {
         item: 'Regular Polish Pedicure',
-        price: '$45',
       },
       {
         item: 'Gel Polish Toe Colour Change',
-        price: '$40',
       },
       {
         item: 'Gel Polish Pedicure',
-        price: '$55',
       },
     ],
   },
